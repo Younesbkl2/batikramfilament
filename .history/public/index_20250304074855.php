a737421355b331cb9ea78d9a21d82826b86a8ca0
Younesbkl2 <younes327@gmail.com>
2025-04-15 10:10:14 +0100

    something again and again and again and again and again and again and again and again

--- .history/public/index_20250304074855.php
+++ .history/public/index_20250304074855.php
@@ -5,22 +5,16 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// Clear any existing output buffers
-while (ob_get_level() > 0) {
-    ob_end_clean();
-}
-
-// Maintenance mode check
+// Determine if the application is in maintenance mode...
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
-// Autoloader
+// Register the Composer autoloader...
 require __DIR__.'/../vendor/autoload.php';
 
-// Bootstrap Laravel
+// Bootstrap Laravel and handle the request...
 /** @var Application $app */
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
-// Handle request
 $app->handleRequest(Request::capture());
